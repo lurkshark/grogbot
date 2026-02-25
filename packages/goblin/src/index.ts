786dab59509f6f066e40871ba25e99fc1fd6cfc2
lurkshark <13933101+lurkshark@users.noreply.github.com>
2026-02-25 11:46:51 -0800

    teardown

--- packages/goblin/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './commands/extract.js';
-export * from './commands/transform.js';
-export * from './commands/load.js';
-export * from './utils/slug.js';
-export * from './utils/frontmatter.js';
-export * from './utils/rss.js';
-export * from './utils/chunking.js';
